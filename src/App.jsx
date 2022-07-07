760430e9ae77bd145455d07776c14b8c6c8b54dd
Alex Angelos <alex_angelos@hotmail.com>
2022-07-06 21:41:01 -0300

    Página de Filmes utilizando API

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar'
+import './App.css'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Outlet />
+    </div>
+  )
+}
+
+export default App
